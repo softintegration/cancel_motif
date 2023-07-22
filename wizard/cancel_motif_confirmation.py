7432b0fcfa5253a09b7a7e960bd3e52d2536a83c
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2023-07-22 16:02:54 +0100

    Cancel motif:Add possibility to hide the motif in the wizard

--- wizard/cancel_motif_confirmation.py
+++ wizard/cancel_motif_confirmation.py
@@ -12,6 +12,8 @@ class CancelMotifConfirmation(models.TransientModel):
     cancel_motif_id = fields.Many2one('cancel.motif',string='Cancel motif')
     cancel_date = fields.Date(string='Cancel date',default=lambda self: fields.Datetime.now())
     display_cancel_date = fields.Boolean(default=False)
+    display_cancel_motif_id = fields.Boolean(default=True)
+    required_cancel_motif_id = fields.Boolean(default=True)
     model = fields.Char(string='Model name')
 
     def process(self):
